a8474dbad5b876b592bee7fd9fbc9c815773869b
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-20 14:06:21 -0400

    Add session provider

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,12 @@
 import "./globals.css";
+import Provider from "@/components/Provider";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Provider>
+        <body>{children}</body>
+      </Provider>
     </html>
   );
 }
