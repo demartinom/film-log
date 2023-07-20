72d76e40bd7f7d9a7518cb83358f0bd10e9c0316
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-20 14:56:21 -0400

    Add Google as provider

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-export const options = {
+const options = {
   providers: [
     GitHubProvider({
-      clientID: process.env.GITHUB_ID,
+      clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   pages: { signin: "/signin" },
