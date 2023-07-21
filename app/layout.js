0302ec0bfd41f25d9dd054f66be419ca1a9ccab8
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-21 13:01:01 -0400

    Correct route

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Provider from "@/components/Provider";
+import Provider from "@/components/provider";
 
 export default function RootLayout({ children }) {
   return (
