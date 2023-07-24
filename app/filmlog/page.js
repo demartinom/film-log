59e3409415b4492c32dc07908ac0b07b4243d54e
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-24 13:31:54 -0400

    Update component name

--- app/filmlog/page.js
+++ app/filmlog/page.js
@@ -3,10 +3,10 @@ import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import { OPTIONS } from "../api/auth/[...nextauth]/route";
 
-export default async function ExamplePage() {
+export default async function FilmLog() {
   const session = await getServerSession(OPTIONS);
   if (!session) {
-    redirect("/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F");
+    redirect("/api/auth/signin");
   }
   return <div>Film Log</div>;
 }
