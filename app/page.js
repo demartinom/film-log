8bb34cd7159b5dda75223fa90c58f4be14afe11a
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-25 11:58:15 -0400

    Move login button

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import LoginButton from "@/components/loginButton";
 import { getServerSession } from "next-auth";
 import { OPTIONS } from "./api/auth/[...nextauth]/route";
 
@@ -6,8 +5,7 @@ export default async function Home() {
   const session = await getServerSession(OPTIONS);
   return (
     <main>
-      {session ?<h1>Hello {session.user.name}!</h1>: <h1>Please Sign In!</h1>}
-      <LoginButton />
+      {session ? <h1>Hello {session.user.name}!</h1> : <h1>Please Sign In!</h1>}
     </main>
   );
 }
