ffcdd2c133c749d2bf53de78298d90e585ca0056
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-25 13:11:01 -0400

    Create dummy table

--- app/filmlog/page.js
+++ app/filmlog/page.js
@@ -8,5 +8,42 @@ export default async function FilmLog() {
   if (!session) {
     redirect("/api/auth/signin");
   }
-  return <div>Film Log</div>;
+  return (
+    <div>
+      <h1>Film Log</h1>
+      <div>
+        <table className="table w-1/2">
+          <thead>
+            <tr>
+              <th>Film Stock</th>
+              <th># of Rolls</th>
+              <th>ISO</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="hover">
+              <td>UltraMax</td>
+              <td>3</td>
+              <td>400</td>
+            </tr>
+            <tr className="hover">
+              <td>Gold</td>
+              <td>2</td>
+              <td>200</td>
+            </tr>
+            <tr className="hover">
+              <td>Lomo 400</td>
+              <td>5</td>
+              <td>400</td>
+            </tr>
+            <tr className="hover">
+              <td>Lomo 800</td>
+              <td>5</td>
+              <td>800</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
