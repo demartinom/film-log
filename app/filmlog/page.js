b1c04db52e203e8f4f073979d34d250fa6d21e46
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-07-28 11:46:14 -0400

    Edit film stock table

--- app/filmlog/page.js
+++ app/filmlog/page.js
@@ -2,12 +2,35 @@ import React from "react";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import { OPTIONS } from "../api/auth/[...nextauth]/route";
+import { PrismaClient } from "@prisma/client";
+import NewFilm from "@/components/newFilm";
+
+const prisma = new PrismaClient();
+
+async function getFilm(user) {
+  try {
+    const rollData = await prisma.roll.groupBy({
+      by: ["filmStockId"],
+      _count: { filmStockId: true },
+    });
+    const filmData = await prisma.filmStock.findMany({
+      include: {
+        Roll: { where: { user: user } },
+        maker: true,
+      },
+    });
+    return { rollCount: rollData, film: filmData };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
 
 export default async function FilmLog() {
   const session = await getServerSession(OPTIONS);
   if (!session) {
     redirect("/api/auth/signin");
   }
+  const filmData = await getFilm(session.user.email);
   return (
     <div>
       <h1>Film Log</h1>
@@ -17,32 +40,33 @@ export default async function FilmLog() {
             <tr>
               <th>Film Stock</th>
               <th># of Rolls</th>
+              <th>Film Maker</th>
               <th>ISO</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr className="hover">
-              <td>UltraMax</td>
-              <td>3</td>
-              <td>400</td>
-            </tr>
-            <tr className="hover">
-              <td>Gold</td>
-              <td>2</td>
-              <td>200</td>
-            </tr>
-            <tr className="hover">
-              <td>Lomo 400</td>
-              <td>5</td>
-              <td>400</td>
-            </tr>
-            <tr className="hover">
-              <td>Lomo 800</td>
-              <td>5</td>
-              <td>800</td>
-            </tr>
+            {filmData.film.map(
+              (film) =>
+                film.Roll.length > 0 && (
+                  <tr className="hover" key={film.id}>
+                    <td>{film.name}</td>
+                    <td>
+                      {
+                        film.Roll.filter(
+                          (filmStock) => filmStock.filmStockId == film.id
+                        ).length
+                      }
+                    </td>
+                    <td>{film.maker.name}</td>
+                    <td>{film.ISO}</td>
+                    <td>See your rolls</td>
+                  </tr>
+                )
+            )}
           </tbody>
         </table>
+            <NewFilm/>
       </div>
     </div>
   );
