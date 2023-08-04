13b137c6c2f25c95e743d71248e4189a6ae4e97a
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-08-04 13:34:40 -0400

    Add table of current rolls

--- app/filmlog/myfilm/[filmstockid]/page.js
+++ app/filmlog/myfilm/[filmstockid]/page.js
@@ -6,18 +6,57 @@ import { OPTIONS } from "@/app/api/auth/[...nextauth]/route";
 const prisma = new PrismaClient();
 
 async function getFilm(session, filmID) {
-  const data = await prisma.roll.findMany({
-    where: {
-      user: session.user.email,
-      filmStockId: filmID,
-    },
-    include: { film: true, maker: true },
-  });
-  return data;
+  try {
+    const data = await prisma.roll.findMany({
+      where: {
+        user: session.user.email,
+        filmStockId: filmID,
+      },
+      include: { film: true, maker: true },
+    });
+    return data;
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+function convertDate(date) {
+  return new Date(date).toLocaleDateString("en-US");
 }
 
 export default async function FilmStock({ params: { filmstockid } }) {
   const session = await getServerSession(OPTIONS);
   const data = await getFilm(session, parseInt(filmstockid));
-  return <div>FilmStock</div>;
+
+  return (
+    <div>
+      <h1>
+        {data[0].maker.name} {data[0].film.name}
+      </h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Roll #</th>
+            <th>Date Started</th>
+            <th>Date Finished</th>
+            <th>Comments</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((roll) => (
+            <tr key={roll.id}>
+              <th>{data.indexOf(roll) + 1}</th>
+              <td>{convertDate(roll.dateStarted)}</td>
+              <td>
+                {roll.dateFinished
+                  ? convertDate(roll.dateFinished)
+                  : "In Progress"}
+              </td>
+              <td>{roll.comments}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
