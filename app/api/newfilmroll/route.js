d8f9d41bb6853ab14eaf916cfa0b3e8a63481f77
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-08-10 13:03:07 -0400

    Create route for new roll

---
+++ app/api/newfilmroll/route.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(request) {
+  try {
+    const req = await request.json();
+    let filmStock = await prisma.filmStock.findUnique({
+      where: { id: parseInt(req.filmStockID) },
+      include: { maker: true },
+    });
+    const data = await prisma.roll.create({
+      data: {
+        film: { connect: { id: filmStock.id } },
+        maker: { connect: { id: filmStock.maker.id } },
+        user: req.user,
+        dateStarted: new Date(req.dateStarted).toISOString(),
+        dateFinished: req.dateFinished
+          ? new Date(req.dateFinished).toISOString()
+          : null,
+        comments: req.comments ?? null,
+      },
+    });
+    return NextResponse.json(data);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
