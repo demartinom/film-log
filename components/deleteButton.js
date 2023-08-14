c819886186647c9d6c75ad468618355daf725a7b
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-08-14 15:37:42 -0400

    Add functionality to delete roll

--- components/deleteButton.js
+++ components/deleteButton.js
@@ -1,21 +1,54 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaDeleteLeft } from "react-icons/fa6";
 
-async function deleteRoll(rollID) {
-  await fetch("http://localhost:3000/api/deleteroll", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(rollID),
-  });
-}
-
 export default function DeleteButton({ rollID }) {
+  async function deleteRoll(rollID) {
+    try {
+      setWait(true);
+      await fetch("http://localhost:3000/api/deleteroll", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(rollID),
+      });
+    } finally {
+      setAlertState(true);
+      setTimeout(() => {
+        setAlertState(false);
+      }, 3000);
+    }
+  }
+  const [alertState, setAlertState] = useState(false);
+  const [wait, setWait] = useState(false);
   return (
-    <button>
-      <FaDeleteLeft onClick={() => deleteRoll(rollID)} />
-    </button>
+    <>
+      {wait && (
+        <div
+          className={`absolute -top-16 -right-44 alert alert-success ${
+            alertState ? "opacity-100" : "opacity-0"
+          } transition-all duration-300`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 stroke-current shrink-0"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>Roll Deleted!</span>
+        </div>
+      )}
+      <button>
+        <FaDeleteLeft onClick={() => deleteRoll(rollID)} oncli />
+      </button>
+    </>
   );
 }
