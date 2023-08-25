ed11b03e7f4f8dfbc4e6f134155f2919c8accbd3
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-08-25 13:36:54 -0400

    Add format to table title

--- app/filmlog/myfilm/[filmstockid]/page.js
+++ app/filmlog/myfilm/[filmstockid]/page.js
@@ -36,9 +36,10 @@ export default async function FilmStock({ params: { filmstockid } }) {
   return (
     <div className="relative px-5 mt-5">
       <BackButton />
-      <h1 className="pt-3 mb-10 text-3xl text-center">
+      <h1 className="pt-3 text-3xl text-center">
         {data[0].maker.name} {data[0].film.name} {data[0].film.ISO}
       </h1>
+      <h2 className="mt-2 mb-10 text-lg text-center">{data[0].film.format}</h2>
       <div className="overflow-x-auto">
         <table className="table text-center md:table-md table-xs">
           <thead>
