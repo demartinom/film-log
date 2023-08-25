fb2cd6f4cd74d6e621d5cb875c4b7feaeac12a02
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-08-25 12:06:54 -0400

    Make button responsive

--- components/backButton.js
+++ components/backButton.js
@@ -1,17 +1,27 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa6";
 
 export default function BackButton() {
   const router = useRouter();
 
   return (
-    <button
-      type="button"
-      className="absolute btn btn-md top-3"
-      onClick={() => router.push("/filmlog")}
-    >
-      Return to Film Log
-    </button>
+    <>
+      <button
+        type="button"
+        className="absolute hidden btn btn-md top-3 md:inline-flex"
+        onClick={() => router.push("/filmlog")}
+      >
+        Return to Film Log
+      </button>
+      <button
+        type="button"
+        className="absolute btn btn-sm top-3 md:hidden"
+        onClick={() => router.push("/filmlog")}
+      >
+        <FaArrowLeft />
+      </button>
+    </>
   );
 }
